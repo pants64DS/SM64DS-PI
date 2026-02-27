529cf2fd29920763a3200a9178c38dd65f27e3c2
Hodgeblossom <ttfg06@gmail.com>
2026-02-27 16:49:54 -0500

    Add: StageList.h

--- include/Lists.h
+++ include/Lists.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "Lists/FileList.h"
-#include "Lists/SoundList.h"
 #include "Lists/ActorList.h"
+#include "Lists/FileList.h"
 #include "Lists/MusicList.h"
+#include "Lists/SoundList.h"
+#include "Lists/StageList.h"
